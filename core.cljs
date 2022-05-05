99d490f86a946aaece3644f90e1fd54103a23834
sakib <nsakib.cse@gmail.com>
2022-05-06 01:27:28 +0600

    problem-3

--- core.cljs
+++ core.cljs
@@ -4,7 +4,9 @@
 (def problem-2
   (= (- 10 (* 2 3)) 4))
 
-(assert (every? true? [problem-1 problem-2]))
+(def problem-3 (= "HELLO WORLD" (.toUpperCase "hello world")))
+
+(assert (every? true? [problem-1 problem-2 problem-3]))
 
 (comment
   problem-1
